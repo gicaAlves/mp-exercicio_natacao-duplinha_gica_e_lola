c4894ea851329906ebea44147aae75c7f505169d
Giovanna Alves <giov.alves@outlook.com>
2022-10-27 19:03:29 -0300

    adicionar comentários

--- natacao.js
+++ natacao.js
@@ -1,9 +1,10 @@
 //exercício Natação em dupla
 //feito por Gica e Lola (Giovanna Alves e Paola)
 
+//variavel da idade da pessoa competidora
 var idade = 4
 
-
+//estrutura de condição das categorias 
 if (idade >= 5 && idade <= 7){
     console.log("Infantil A")
 } else if (idade >= 8 && idade <= 11){
